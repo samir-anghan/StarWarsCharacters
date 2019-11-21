33b9fe720a0c7e9373de050fd80996b46bbf5f2b
Samir Anghan <samir.anghan@gmail.com>
2019-11-20 19:03:46 -0500

    Initial Commit

---
+++ StarWarsCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsCharacters
+//
+//  Created by Dev on 11/20/19.
+//  Copyright © 2019 Samir Anghan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
